bd8855ed9f4a3969ddb44926f714bbe4a3c9eb6d
Anusheel Bhushan <anusheel@users.noreply.github.com>
2023-11-04 05:01:53 -0700

    Commit from Stub AI (#5)

---
+++ components/SubmissionsList.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+
+type Submission = {
+  name: string;
+  dob: string;
+};
+
+const SubmissionsList = () => {
+  const [submissions, setSubmissions] = useState<Submission[]>([]);
+
+  useEffect(() => {
+    fetch('/api/submissions')
+      .then((response) => response.json())
+      .then((data) => setSubmissions(data));
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full max-w-md mx-auto mt-5">
+      <h2 className="mb-4 text-lg font-semibold text-gray-700 dark:text-gray-200">
+        Previous Submissions
+      </h2>
+      <div className="overflow-x-auto">
+        <table className="w-full max-w-md mx-auto mt-5 text-center bg-white rounded-lg shadow-md overflow-hidden">
+          <thead className="bg-gray-200">
+            <tr>
+              <th className="px-4 py-2">Name</th>
+              <th className="px-4 py-2">Date of Birth</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white">
+            {submissions.map((submission, index) => (
+              <tr className={index % 2 === 0 ? 'bg-gray-100' : ''} key={index}>
+                <td className="px-4 py-2">{submission.name}</td>
+                <td className="px-4 py-2">{submission.dob}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default SubmissionsList;
