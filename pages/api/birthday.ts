7bb74cd2581327b9161dfeea916558075f1c6943
anusheel <anusheel@gmail.com>
2023-11-04 12:10:37 +0000

    Commit from Stub AI

--- pages/api/birthday.ts
+++ pages/api/birthday.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -6,11 +5,13 @@ type Data = {
   dob: string
 }
 
+export let submissions: Data[] = [];
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   const { name, dob } = req.body;
-  // Here you can add your logic to save the data in your database
+  submissions.push({ name, dob });
   res.status(200).json({ name, dob });
 }
